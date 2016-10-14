8b864257433d0192bd666cdda304846069e2f3aa
Birkir Brynjarsson <birkir@Birkirs-MacBook-Air.local>
2016-10-14 22:29:37 +0000

    throws exception for negatives, doesnt handle newlines anymore

--- src/main/java/is/ru/stringcalculator/StringCalculator.java
+++ src/main/java/is/ru/stringcalculator/StringCalculator.java
@@ -2,14 +2,27 @@ package is.ru.stringcalculator;
 
 public class StringCalculator {
 
-	public static int add(String text){
+	public static int add(String text) throws Exception {
 		if(text == "")
 			return 0;
 		String[] numbers = text.split(",");
 		int sum = 0;
 		for(String n : numbers){
-			sum += Integer.parseInt(n);
+			int num = Integer.parseInt(n);
+			if(num < 0){
+				String negatives = handleNegatives(numbers);
+				throw new Exception("Negatives not allowed: " + negatives);
+			}
+			sum += num;
 		}
 		return sum;
+	}
+
+	public static String handleNegatives(String[] s){
+		String negatives = "";
+		for(String n : s)
+			if(Integer.parseInt(n) < 0)
+				negatives += (n + ",");
+		return negatives.substring(0, negatives.length()-1);
 	}
 }
--- src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/StringCalculatorTest.java
@@ -6,28 +6,38 @@ import org.junit.Test;
 public class StringCalculatorTest {
 	
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() throws Exception {
 		assertEquals(0, StringCalculator.add(""));
 	}
 
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber() throws Exception {
 		assertEquals(1, StringCalculator.add("1"));
 	}
 
 	@Test
-	public void testTwoNumbers() {
+	public void testTwoNumbers() throws Exception {
 		assertEquals(3, StringCalculator.add("1,2"));
 	}
 
 	@Test
-	public void TestMultipleNumbers() {
-		assertEquals(798, StringCalculator.Add("13,12,53,566,43,21,3,87"));
+	public void TestMultipleNumbers() throws Exception {
+		assertEquals(798, StringCalculator.add("13,12,53,566,43,21,3,87"));
 	}
 
 	@Test
-	public void TestNewlines() {
-		assertEquals(6, StringCalculator.Add("1\n2,3"));
+	public void TestNewlines() throws Exception {
+		assertEquals(6, StringCalculator.add("1\n2,3"));
 	}
-}
+
+	@Test
+	public void TestNegatives() throws Exception {
+		try {
+			assertEquals(-4, StringCalculator.add("2,-4,3,-5"));
+		}
+		catch (Exception e) {
+			String expectedMessage = "Negatives not allowed: -4,-5";
+			assertEquals(expectedMessage, e.getMessage());
+		}
+	}
 }
